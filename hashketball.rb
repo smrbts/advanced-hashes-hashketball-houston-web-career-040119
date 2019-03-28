87d1dd29d8b2ae6d9e0427ed7c902cd9a4c83bfd
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 19:49:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,18 @@
 def game_hash
   game_hash = 
   {
-    
+    home:
+    {
+      team_name:{},
+      colors:{},
+      players:{}
+    },
+    away:
+    {
+      team_name:{},
+      colors:{},
+      players:{}
+    }
   }
 end
 
