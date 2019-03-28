63578b2a4f278cea359f045a607e7e96ea62bcdf
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 20:14:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -30,9 +30,19 @@ def game_hash
   }
 end
 
-def num_points_scored
+def num_points_scored(player_name)
+  
 end
 
+def shoe_size(player_name)
+  
+end
+
+def team_colors(team_name)
+  
+end
+
+def team_names
 
 
 
