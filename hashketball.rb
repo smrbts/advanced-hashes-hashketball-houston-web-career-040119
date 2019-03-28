adf445ea66dac58d278590f8d85e9dc268fb01df
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 20:25:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -34,7 +34,10 @@ def num_points_scored(player_name)
   game_hash.each do |team, team_data|
     team_data[:players].each do |name, stats|
       if name == player_name
-        return 
+        return stats[:points]
+      end
+    end
+  end
 end
 
 def shoe_size(player_name)
