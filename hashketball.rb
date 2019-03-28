3cd8b68e9e08ec7e1233e269ef1faa4156000e38
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 19:55:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,8 +7,15 @@ def game_hash
       colors:["Black", "White"],
       players:
       [
-        {player_name: "Alan Anderson"
-          
+        { player_name: "Alan Anderson"
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
         },
       ]
     },
