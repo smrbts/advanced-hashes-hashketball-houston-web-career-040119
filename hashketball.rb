b2e7696ce951e7a390402125fd37be5e8b63e665
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 20:43:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,7 +63,7 @@ def team_names
 end
 
 def player_numbers(team_name)
-  find_team(team_name)[:players].collect { |team| team[]
+  find_team(team_name)[:players].collect { |team| team[:number] }
 end
 
 def player_stats(player_name)
