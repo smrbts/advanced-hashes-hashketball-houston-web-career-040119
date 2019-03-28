93a36900e4001c1731c52294b06ce860a66bf564
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 20:09:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -24,7 +24,7 @@ def game_hash
         "Bismak Biyombo":{number:0, shoe:16, points:12, rebounds:4, assists:7, steals:7, blocks:15, slam dunks:10},
         "DeSagna Diop":{number:2, shoe:14, points:24, rebounds:12, assists:12, steals:4, blocks:5, slam dunks:5},
         "Ben Gordon":{number:8, shoe:15, points:33, rebounds:3, assists:2, steals:1, blocks:1, slam dunks:0},
-        "Brendan Haywood":{number:31, shoe:15, points:19, rebounds:2, assists:2, steals:4, blocks:11, slam dunks:1}
+        "Brendan Haywood":{number:33, shoe:15, points:6, rebounds:12, assists:12, steals:22, blocks:5, slam dunks:12}
       ]
     }
   }
