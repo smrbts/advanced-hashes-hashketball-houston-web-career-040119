3aa573855c02d1b555347340a83fa10c3a5ca841
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 19:54:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,13 +7,15 @@ def game_hash
       colors:["Black", "White"],
       players:
       [
-        {player_name: "Alan Anderson"}
+        {player_name: "Alan Anderson"
+          
+        },
       ]
     },
     away:
     {
-      team_name:{},
-      colors:[],
+      team_name:"Charlotte Hornets",
+      colors:["Turqoise", "Purple"],
       players:
       [
         {}
