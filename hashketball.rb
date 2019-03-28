30bb89a3956d0bc26ae69c6bbd6686b67a0b9d03
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 20:45:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -67,7 +67,7 @@ def player_numbers(team_name)
 end
 
 def player_stats(player_name)
-  find_player(player_name).delete_if { |key, values| key == 
+  find_player(player_name).delete_if { |key, values| key == :player_name }
 end
 
 def big_shoe_rebounds
