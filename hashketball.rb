078efd53e29d75881022929d90c2fb0a050b5485
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 21:05:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -31,7 +31,7 @@ def game_hash
     away:
     {
       team_name:"Charlotte Hornets",
-      colors:["Turqoise", "Purple"],
+      colors:["Turquoise", "Purple"],
       players:
       [
         {player_name: "Jeff Adrien", number:4, shoe:18, points:10, rebounds:1, assists:1, steals:2, blocks:7, slam_dunks:2},
