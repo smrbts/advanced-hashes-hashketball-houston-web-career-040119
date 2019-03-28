81930a2c165327b100ec0e5d60aafa6a632ab47a
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 20:08:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -20,7 +20,11 @@ def game_hash
       colors:["Turqoise", "Purple"],
       players:
       [
-        {}
+        "Jeff Adrien":{number:4, shoe:18, points:10, rebounds:1, assists:1, steals:2, blocks:7, slam dunks:2},
+        "Bismak Biyombo":{number:0, shoe:16, points:12, rebounds:4, assists:7, steals:7, blocks:15, slam dunks:10},
+        "DeSagna Diop":{number:2, shoe:14, points:24, rebounds:12, assists:12, steals:4, blocks:5, slam dunks:5},
+        "Ben Gordon":{number:8, shoe:15, points:33, rebounds:3, assists:2, steals:1, blocks:1, slam dunks:0},
+        "Brendan Haywood":{number:31, shoe:15, points:19, rebounds:2, assists:2, steals:4, blocks:11, slam dunks:1}
       ]
     }
   }
