cc59c764ee802d63868a1b4f72937c6913328dad
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 21:00:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -21,7 +21,7 @@ def game_hash
       colors:["Black", "White"],
       players:
       [
-        "Alan Anderson":{number:0, shoe:16, points:22, rebounds:12, assists:12, steals:3, blocks:1, slam_dunks:1},
+        player_name: "Alan Anderson" number:0, shoe:16, points:22, rebounds:12, assists:12, steals:3, blocks:1, slam_dunks:1},
         "Reggie Evans":{number:30, shoe:14, points:12, rebounds:12, assists:12, steals:12, blocks:12, slam_dunks:7},
         "Brook Lopez":{number:11, shoe:17, points:17, rebounds:19, assists:10, steals:3, blocks:1, slam_dunks:15},
         "Mason Plumlee":{number:1, shoe:19, points:26, rebounds:12, assists:6, steals:3, blocks:8, slam_dunks:5},
